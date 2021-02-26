053b75f3a53c174b16714ed649b87a42467e47ef
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-02-26 18:09:52 +0530

    Added awake server and removed myStock model

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const axios = require("axios");
 const app = require("./src/app")
 const port = process.env.PORT
 app.listen(port, () => console.log('server is listening on', port))
+
+const awakeServer = () =>
+  axios.get('https://shivi-awake-workers.herokuapp.com')
+
+setInterval(awakeServer, 300000)
